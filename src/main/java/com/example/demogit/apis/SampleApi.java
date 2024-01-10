aba0bccb32f3029f8a0ca59b1c5be632c02c478c
Nageswar2002 <kandlakunta.venkatanageswararao@lumen.com>
2024-01-10 12:53:49 +0530

    api Added

---
+++ src/main/java/com/example/demogit/apis/SampleApi.java
@@ -0,0 +1,16 @@
+package com.example.demogit.apis;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/sample")
+public class SampleApi {
+    @GetMapping
+    public ResponseEntity<String> get()
+    {
+        return ResponseEntity.ok("hello from SampleApi");
+    }
+}
